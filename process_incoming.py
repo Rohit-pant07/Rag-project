3ae89248c5fe4fe43d8a2e4a0ea3ef46bb0bb283
Rohit-pant07 <rohit.pant.ug24@nsut.ac.in>
2025-12-18 20:25:37 +0530

    attached the local llm and made some major changes

--- process_incoming.py
+++ process_incoming.py
@@ -14,20 +14,51 @@ def create_embedding(text_list):
     })
     embedding=r.json()['embeddings']
     return embedding    
+def inference(prompt):
+    r = requests.post(
+    "http://localhost:11434/api/generate",
+    json={
+        "model": "llama3.2",
+        "prompt": prompt,
+        "stream": False
+    }
+)
+    return r.json()
+ 
+    print("Inference Response:", r.json())
+        
 
 
 incoming_queries = input("Enter your query: ")
 query_embedding = create_embedding([incoming_queries])[0]
 # print("Query Embedding:", query_embedding)
 
-print(np.vstack(df['embedding'].values))
-print(np.vstack(df['embedding']).shape)
+# print(np.vstack(df['embedding'].values))
+# print(np.vstack(df['embedding']).shape)
 similarities = cosine_similarity(np.vstack(df['embedding']), [query_embedding])
 # print("Similarities:", similarities)
 top_rusults=30
 top_results_indices = np.argsort(similarities[:,0])[::-1][:top_rusults]
 # print("Top 3 similar chunks indices:", top_results_indices)
 new_df = df.iloc[top_results_indices]
-print(new_df[['number','title', 'text']])
-for index,item in new_df.iterrows():
-    print(index,item["number"],item['title'],item['text'],item['start'],item['end'])
+# print(new_df[['number','title', 'text']])
+# for index,item in new_df.iterrows():
+    # print(index,item["number"],item['title'],item['text'],item['start'],item['end'])
+prompt=f"""
+user has asked this query '{incoming_queries}'
+related to videos chunks contain  the following content:
+{new_df[['number','title', 'text','start','end']].to_json(orient='records')}
+-----------------------------
+you have to answer the query based on the where and how much content is  taught in the video chunks thats is videos. and where at what time stamps
+and guide the user to watch that particular part of the video and recommend the title of video. sound like a helpful teaching assistant and dont mention chunks etc 
+if you are not able to find anything relevant from the chunks then you can say "sorry i dont have that information"
+tell him you can only provide information related to the content present in this course videos.
+make it consise with relevent information.
+
+"""
+# with open("prompt.txt","w") as f:
+#     f.write(prompt)
+response=inference(prompt)['response']
+print("Response:", response)
+with open("response.txt","w") as f:
+    f.write(response)
